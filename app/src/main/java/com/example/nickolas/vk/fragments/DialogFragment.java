a221c679891f9d1d84c134441e7a3ca2bc9e3bf3
Nickolas Humeniuk <kolin98111@gmail.com>
2017-10-01 00:46:13 +0300

    renamed message fragment to dialog fragment

---
+++ app/src/main/java/com/example/nickolas/vk/fragments/DialogFragment.java
@@ -0,0 +1,25 @@
+package com.example.nickolas.vk.fragments;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nickolas.vk.R;
+
+
+public class DialogFragment extends Fragment {
+
+    public static DialogFragment newInstance() {
+        return new DialogFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dialog, container, false);
+    }
+
+}
