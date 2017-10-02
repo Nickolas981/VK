13cfc8df3aca5409378c8f82877e32140c30b75c
Nickolas Humeniuk <kolin98111@gmail.com>
2017-10-02 15:35:49 +0300

    Added rx Actions

---
+++ app/src/main/java/com/example/nickolas/vk/utils/rx/RxErrorAction.java
@@ -0,0 +1,28 @@
+package com.example.nickolas.vk.utils.rx;
+
+import android.content.Context;
+import android.widget.Toast;
+
+
+import com.example.nickolas.vk.R;
+
+import rx.functions.Action1;
+
+
+public class RxErrorAction implements Action1<Throwable> {
+
+    private final Context context;
+
+    public RxErrorAction(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void call(Throwable throwable) {
+        throwable.printStackTrace();
+
+        Toast.makeText(context, context.getString(R.string.request_execution_has_failed),
+                Toast.LENGTH_SHORT).show();
+    }
+
+}
