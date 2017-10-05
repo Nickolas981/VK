26612e7f93e04e6f9e315538f035cf333526c204
Nickolas Humeniuk <kolin98111@gmail.com>
2017-10-05 03:19:53 +0300

    Now fragment don`t reload

--- app/src/main/java/com/example/nickolas/vk/widgets/listners/CustomNavigationBarListner.java
+++ app/src/main/java/com/example/nickolas/vk/widgets/listners/CustomNavigationBarListner.java
@@ -1,6 +1,7 @@
 package com.example.nickolas.vk.widgets.listners;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -14,12 +15,17 @@ import com.example.nickolas.vk.fragments.MusicFragment;
 public class CustomNavigationBarListner implements BottomNavigationView.OnNavigationItemSelectedListener {
 
 
+    Fragment currentFragment;
+    int id;
     private FragmentTransaction fragT;
     private Activity activity;
     private FeedFragment feedFragment;
     private MusicFragment musicFragment;
     private DialogFragment dialogFragment;
-    int id;
+    private Bool dialogInit;
+    private Bool feedInit;
+    private Bool musicInit;
+
 
     public CustomNavigationBarListner(Activity activity, FeedFragment feedFragment, MusicFragment musicFragment, DialogFragment dialogFragment, int id) {
         this.activity = activity;
@@ -27,26 +33,49 @@ public class CustomNavigationBarListner implements BottomNavigationView.OnNaviga
         this.musicFragment = musicFragment;
         this.dialogFragment = dialogFragment;
         this.id = id;
-    }
+        dialogInit = new Bool();
+        feedInit = new Bool();
+        musicInit = new Bool();
 
+//        fragT.add(musicFragment, musicFragment.getTag()).add(feedFragment, feedFragment.getTag())
+//                .add(dialogFragment, dialogFragment.getTag());
+    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         fragT = activity.getFragmentManager().beginTransaction();
         switch (item.getItemId()) {
             case R.id.navigation_music:
-                fragT.replace(id, musicFragment);
-                fragT.commit();
+                changeFragment(musicInit, musicFragment);
+//                fragT.replace(id, musicFragment, musicFragment.getTag());
+//                fragT.commit();
                 return true;
             case R.id.navigation_feed:
-                fragT.replace(id, feedFragment);
-                fragT.commit();
+                changeFragment(feedInit, feedFragment);
+//                fragT.replace(id, feedFragment, feedFragment.getTag());
+//                fragT.commit();
                 return true;
             case R.id.navigation_messages:
-                fragT.replace(id, dialogFragment);
-                fragT.commit();
+                changeFragment(dialogInit, dialogFragment);
+//                fragT.replace(id, dialogFragment, dialogFragment.getTag());
+//                fragT.commit();
                 return true;
         }
         return false;
     }
+
+    private void changeFragment(Bool init, android.app.Fragment fragment) {
+        if (!init.b) {
+            fragT.add(id, fragment).commit();
+            init.b = true;
+        } else {
+            fragT.hide(currentFragment).show(fragment).commit();
+        }
+        currentFragment = fragment;
+    }
+
+    private class Bool {
+        public boolean b;
+    }
+
 }
